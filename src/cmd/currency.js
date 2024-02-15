4b618e8c9af820852d35c8cd332af41831aa7071
Kirdow <10155603+Kirdow@users.noreply.github.com>
2024-02-15 11:05:16 +0000

    fix formatting in currency command

--- src/cmd/currency.js
+++ src/cmd/currency.js
@@ -4,7 +4,9 @@ const currencyCommand = new SlashCommandBuilder()
     .setName('currency')
     .setDescription('Create a currency prompt!')
     .addStringOption((option) =>
-        option.setName('prompt').setDescription('Currency prompt!')
+        option
+            .setName('prompt')
+            .setDescription('Currency prompt')
     )
 
 export default currencyCommand.toJSON()
